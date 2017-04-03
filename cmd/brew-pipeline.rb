d2ab4b9dba3e959782035bba47bf790d4781becc
Samantha Marshall <me@samdmarshall.com>
2017-04-02 23:51:52 -0400

    adding subcommands

--- cmd/brew-pipeline.rb
+++ cmd/brew-pipeline.rb
@@ -3,4 +3,14 @@ require 'claide'
 require 'octokit'
 
 args = CLAide::ARGV.new(ARGV)
-puts args
+
+command = args.shift_arguments
+
+case command
+when 'new'
+  puts 'creating a new formula!'
+when 'update'
+  puts 'updating an existing formula!'
+else
+  puts 'Invalid command issued!'
+  Kernel.exit
